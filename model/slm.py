9569422eb807cfac277db60b8db01d53ce03a080
Debby Powell <debby.powell@ed.ac.uk>
2018-08-30 17:31:41 +0100

    fixed dev rates

--- model/slm.py
+++ model/slm.py
@@ -48,8 +48,7 @@
 #Fish death rate: sum of constant *****background rate=??**** and individual 
 ##lice dependent term *****[INSERT APPROPRIATE DISTRIBUTION]**** ->
 ##sample with prob=rate_i/rate_tot
-#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+#----------------------------------------------------------------------------------------------
 import pandas as pd
 import numpy as np
 from scipy import stats
@@ -58,7 +57,8 @@ from math import exp, log
 
 np.random.seed(545836870)
 
-#Functions----------------------------------------------------------------------------------------------------------------------------------
+#Functions-------------------------------------------------------------------------------------
+
 #random truncated normal variates with more easily readable inputs
 def ranTruncNorm(mean=0, sd=1, low=0, upp=10, length=1):    
     return stats.truncnorm.rvs((low - mean)/sd, (upp - mean)/sd, 
@@ -69,51 +69,74 @@ def devTimeAldrin(del_p, del_m10, del_s, temp_c, D):
     unbounded = log(2)*log(del_s)*D**(del_s-1)*(del_m10*10**del_p/temp_c**del_p)**(-del_s)
     return min(unbounded,1)
 
-#-------------------------------------------------------------------------------------------------------------------------------------------
-    
-#Input Data---------------------------------------------------------------------------------------------------------------------------------
-oban_avetemp = [8.2,7.5,7.4,8.2,9.6,11.3,13.1,13.7,13.6,12.8,11.7,9.8] #www.seatemperature.org
+def aveDevDays(del_p, del_m10, del_s, temp_c):
+    return 100*devTimeAldrin(del_p, del_m10, del_s, temp_c, 100)\
+    -0.001*sum([devTimeAldrin(del_p, del_m10, del_s, temp_c, i) 
+    for i in np.arange(0,100.001,0.001)])
+#----------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------
 
-#Initial Values----------------------------------------------------------------------------------------------------------------------------
-t = 0        #Day 0
+
+#Input Data------------------------------------------------------------------------------------
+oban_avetemp = [8.2,7.5,7.4,8.2,9.6,11.3,13.1,13.7,13.6,12.8,11.7,9.8] #www.seatemperature.org
+mort_rates = [0.17, 0.22, 0.008, 0.05, 0.02, 0.06] # L1,L2,L3,L4,L5f,L5m
 start_date = dt.datetime(2016, 3, 1)
-cur_date = start_date
 tau = 1 ###############################################
+
+#Initial Values--------------------------------------------------------------------------------
+t = 0        #Day 0
+cur_date = start_date
+licepcage0 = 12
 
 #Initial lice population
 lice = pd.DataFrame(columns=['Farm','Cage','Fish','MF','stage','stage_age','resistanceT1'])
-lice['Farm'] = np.repeat(1,12)
-lice['Cage'] = np.repeat(1,12)
+lice['Farm'] = np.repeat(1,licepcage0)
+lice['Cage'] = np.repeat(1,licepcage0)
 lice['Fish'] = np.repeat(range(1,4),4)
-lice['MF'] = np.random.choice(['F','M'],12,p=[0.45,0.55])
+lice['MF'] = np.random.choice(['F','M'],licepcage0,p=[0.45,0.55])
 #Based on median development time at 10 degrees from Aldrin et al 2017
-lice['stage'] = np.random.choice(['L1','L2'],12,p=[0.55,0.45]) 
+lice['stage'] = np.random.choice([1,2],licepcage0,p=[0.55,0.45]) 
 
 
-p=stats.poisson.pmf(range(8),3)
+p=stats.poisson.pmf(range(15),3)
 p[2] = p[2] + 1-sum(p) #probs need to add up to one 
-lice['stage_age'] = np.random.choice(range(8),12,p=p)
+lice['stage_age'] = np.random.choice(range(15),licepcage0,p=p)
 
-rands = ranTruncNorm(mean=-6, sd=2, low=-100000000, upp=0, length=12)
+rands = ranTruncNorm(mean=-6, sd=2, low=-100000000, upp=0, length=licepcage0)
 lice['resistanceT1'] = [exp(i) for i in rands]
 
-#-----------------------------------------------------------------------------------------------------------------------------------------
+#pre-calc values-------------------------------------------------------------------------------
+
+
+#----------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------
+#--------------------------Simulation----------------------------------------------------------
+#----------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------
 
 cur_date = cur_date + dt.timedelta(days=tau)
-#-----------------------------------------------------------------------------------------------------------------------------------------
-#Events during tau in cage----------------------------------------------------------------------------------------------------------------
-#-----------------------------------------------------------------------------------------------------------------------------------------
+lice.stage_age = lice.stage_age + tau
 
-#Development rates------------------------------------------------------------------------------------------------------------------------
-temp_now = oban_avetemp[cur_date.month-1]
-L1toL2 = sum([devTimeAldrin(0.401,8.814,18.869,temp_now,i+tau) 
-             for i in lice.loc[:].stage_age]) 
-#replace ':' with lice['Farm']==farm & lice['Cage']==cage when they have more than 1 value
+#----------------------------------------------------------------------------------------------
+#Events during tau in cage---------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------
+
+
+#Expected number of dev events-----------------------------------------------------------------
+temp_now = oban_avetemp[cur_date.month-1] #change when farm location is available
+L1toL2 = sum([devTimeAldrin(0.401,8.814,18.869,temp_now, i) 
+             for i in lice.loc[lice['stage']=='L1'].stage_age])
+#lice['Farm']==farm & lice['Cage']==cage when they have more than 1 value
     
-L3toL4 = sum([devTimeAldrin(1.305,18.934,7.945,temp_now,i+tau) 
-             for i in lice.loc[:].stage_age])
-L4toL5 = sum([devTimeAldrin(0.866,10.742,1.643,temp_now,i+tau) 
-             for i in lice.loc[:].stage_age])
+L3toL4 = sum([devTimeAldrin(1.305,18.934,7.945,temp_now,i) 
+             for i in lice.loc[lice['stage']=='L3'].stage_age])
+L4toL5 = sum([devTimeAldrin(0.866,10.742,1.643,temp_now,i) 
+             for i in lice.loc[lice['stage']=='L4'].stage_age])
 
+
+#Mortality rates-------------------------------------------------------------------------------
+
+    
+    
 
 
