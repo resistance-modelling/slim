88f7382a9da5216f8b4bb00cfc3d0c70b63142ad
Debby Powell <debby.powell@ed.ac.uk>
2018-09-13 16:52:24 +0100

    fish model and infection events

--- model/slm.py
+++ model/slm.py
@@ -4,33 +4,32 @@
 #Input data: location of cages/farms, (Average) monthly(?) water T at those locations
 
 #Sea lice lifecycle
-#Lice values held in data frame (FILE?) with entries for: farm, cage, fish, MF, 
+#Lice values held in data frame with entries for: farm, cage, fish, MF, 
 ##stage, stage-age, resistanceT1, resitanceT2,...
 #Initialise by setting initial lice population for each cage and sample 
 ##resistance values from distribution for natural occurance of resistance 
-##***[INSERT APPROPRIATE DISTRIBUTION]*** and male/female=1.22 (cox et al 2017)
+##***[INSERT APPROPRIATE DISTRIBUTION]*** and male/female=1.22 (cox et al 2017) maybe 1 
+##as females live longer
 
 
 #For each cage-subset simultaneously:
 #Calculate rate of events
 #For each event sample number of times it happened during a fixed period tau=? 
-##***(to be estimated)*** from Pois(rate*tau)
+##***(to be estimated)*** from Pois(E(events during tau))
 #Sample who they happen to and update value
-##If event is infection sample fish ***density dependent???****
+##If event is infection sample fish 
 ##If event is mating sample both and create new entries with status 0 and 
 ###create resistance values based on parents ***[INSERT MODE OF INHERITANCE]***
 ### 6-11 broods of 500 ova on average (see Costello 2006) extrude a pair every 3 days 
-###***Average number of eggs for all or distribution???****
 #Update fish growth and death (remove all lice entries associated w dead fish) 
 #Migrate lice in L1/2 to other cages/farms within range 11-45km mean 27km Siegal et al 2003
 ##***INSERT FUNCTION***
 
-#Update all values, add seasonal inputs e.g. treatment, wildlife reservoir and 
-##run all cages again
+#Update all values, add seasonal inputs e.g. treatment, wildlife reservoir (input from  
+##reservoir can be changed to model use of skirt/closed cage) and run all cages again
 
 #Lice events:
-#infecting a fish (L2->L3) -> rate= ***[INSERT FUNCTION FISH WEIGHT BY AGE]**** 
-##to be used with function from Aldrin et al 2017
+#infecting a fish (L2->L3) -> rate from Aldrin et al 2017
 #L1->L2,L3->L4,L4->L5 -> rate=sum(prob of dev after A days in a stage as given 
 ##by Aldrin et al 2017)
 #Background death in a stage (remove entry) -> rate= number of individuals in 
@@ -40,14 +39,16 @@
 ##for individuals in appropriate stage)*[TREATMENT EFFECT]*[DOSE PRESENCE/DECAY]****
 #Mating -> rate fish_i= adult males on fish_i*adult females on fish_i*(1/4)*
 ##(2.5/ave adult female lifetime) 
-##Or set lice as unavailable for x days after mating and assume that a female 
+##Set lice as unavailable for 3 days after mating and assume that a female 
 ##will mate as soon as available if there's an available male
 
 #Fish model:
-#Fish growth rate ****[INSERT FUNCTION]**** -> 
-#Fish death rate: sum of constant *****background rate=??**** and individual 
-##lice dependent term *****[INSERT APPROPRIATE DISTRIBUTION]**** ->
-##sample with prob=rate_i/rate_tot
+#Fish growth rate -> 10000/(1+exp(-0.01*(t-475))) fitted logistic curve to data from 
+##http://www.fao.org/fishery/affris/species-profiles/atlantic-salmon/growth/en/
+#Fish death rate: constant background daily rate 0.00057 based on 
+##www.gov.scot/Resource/0052/00524803.pdf 
+##multiplied by lice coefficient see surv.py (compare to threshold of 0.75 lice/g fish)
+
 #----------------------------------------------------------------------------------------------
 import pandas as pd
 import numpy as np
@@ -78,34 +79,40 @@ def aveDevDays(del_p, del_m10, del_s, temp_c):
 
 
 #Input Data------------------------------------------------------------------------------------
-oban_avetemp = [8.2,7.5,7.4,8.2,9.6,11.3,13.1,13.7,13.6,12.8,11.7,9.8] #www.seatemperature.org
-mort_rates = [0.17, 0.22, 0.008, 0.05, 0.02, 0.06] # L1,L2,L3,L4,L5f,L5m
+oban_avetemp = np.array([8.2,7.5,7.4,8.2,9.6,11.3,13.1,13.7,13.6,12.8,11.7,9.8]) #www.seatemperature.org
+ncages = 2
+fish0 = np.array([[3,3]])
+mort_rates = np.array([0.17, 0.22, 0.008, 0.05, 0.02, 0.06]) # L1,L2,L3,L4,L5f,L5m
+fb_mort = 0.00057
+lice_coef = -2.4334
 start_date = dt.datetime(2016, 3, 1)
 tau = 1 ###############################################
 
 #Initial Values--------------------------------------------------------------------------------
 t = 0        #Day 0
 cur_date = start_date
-licepcage0 = 12
+licepfish0 = 8
+cur_fish = fish0.copy() 
 
+lice_tot = np.sum(licepfish0*fish0)
 #Initial lice population
 lice = pd.DataFrame(columns=['Farm','Cage','Fish','MF','stage','stage_age','resistanceT1'])
-lice['Farm'] = np.repeat(1,licepcage0)
-lice['Cage'] = np.repeat(1,licepcage0)
-lice['Fish'] = np.repeat(range(1,4),4)
-lice['MF'] = np.random.choice(['F','M'],licepcage0,p=[0.45,0.55])
+lice['Farm'] = np.repeat(1,lice_tot)
+lice['Cage'] = np.array([np.repeat(i,licepfish0*fish0[j-1][i-1]) 
+                          for i in range(1,ncages+1) for j in lice.Farm.unique()]).flatten()
+lice['Fish'] = np.array([np.tile(range(1,fish0[i-1][j-1]+1),licepfish0) 
+                       for i in lice.Farm.unique() for j in lice.Cage.unique()]).flatten()
+lice['MF'] = np.random.choice(['F','M'],lice_tot)
 #Based on median development time at 10 degrees from Aldrin et al 2017
-lice['stage'] = np.random.choice([1,2],licepcage0,p=[0.55,0.45]) 
+lice['stage'] = np.random.choice([1,2],lice_tot,p=[0.55,0.45]) 
 
 
 p=stats.poisson.pmf(range(15),3)
-p[2] = p[2] + 1-sum(p) #probs need to add up to one 
-lice['stage_age'] = np.random.choice(range(15),licepcage0,p=p)
+p = p/sum(p) #probs need to add up to one 
+lice['stage_age'] = np.random.choice(range(15),lice_tot,p=p)
 
-rands = ranTruncNorm(mean=-6, sd=2, low=-100000000, upp=0, length=licepcage0)
-lice['resistanceT1'] = [exp(i) for i in rands]
-
-#pre-calc values-------------------------------------------------------------------------------
+rands = ranTruncNorm(mean=-6, sd=2, low=-100000000, upp=0, length=lice_tot)
+lice['resistanceT1'] = np.exp(rands)
 
 
 #----------------------------------------------------------------------------------------------
@@ -115,28 +122,42 @@ lice['resistanceT1'] = [exp(i) for i in rands]
 #----------------------------------------------------------------------------------------------
 
 cur_date = cur_date + dt.timedelta(days=tau)
+t = cur_date - start_date
 lice.stage_age = lice.stage_age + tau
+
 
 #----------------------------------------------------------------------------------------------
 #Events during tau in cage---------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
-
+farm = 1 ###############################
+cage = 1 ###############################
+cur_cage = lice.loc[(lice['Farm']==farm) & (lice['Cage']==cage)]
 
 #Expected number of dev events-----------------------------------------------------------------
 temp_now = oban_avetemp[cur_date.month-1] #change when farm location is available
 L1toL2 = sum([devTimeAldrin(0.401,8.814,18.869,temp_now, i) 
-             for i in lice.loc[lice['stage']=='L1'].stage_age])
-#lice['Farm']==farm & lice['Cage']==cage when they have more than 1 value
+             for i in cur_cage.loc[cur_cage['stage']==1].stage_age])
+
     
 L3toL4 = sum([devTimeAldrin(1.305,18.934,7.945,temp_now,i) 
-             for i in lice.loc[lice['stage']=='L3'].stage_age])
+             for i in cur_cage.loc[cur_cage['stage']==3].stage_age])
 L4toL5 = sum([devTimeAldrin(0.866,10.742,1.643,temp_now,i) 
-             for i in lice.loc[lice['stage']=='L4'].stage_age])
+             for i in cur_cage.loc[cur_cage['stage']==4].stage_age])
 
 
-#Mortality rates-------------------------------------------------------------------------------
-
+#Expected number of bg mortality events--------------------------------------------------------
+inds_stage = [sum(cur_cage['stage']==i) for i in range(1,7)]
+Emort = np.multiply(mort_rates, inds_stage)   
     
-    
+#Fish growth and death-------------------------------------------------------------------------
+wt = 10000/(1+exp(-0.01*(t.days-475)))
+adlicepg = np.array([(sum(cur_cage.loc[cur_cage['Fish']==i].stage>3))/wt 
+           for i in cur_cage.Fish.unique()])
+Plideath = 1/(1+np.exp(-19*(adlicepg-0.63)))
+Efish_death = (fb_mort - np.log(1-Plideath))*tau*cur_fish[farm-1][cage-1]
 
+#Infection events------------------------------------------------------------------------------
+eta_aldrin = -2.576 + log(cur_fish[farm-1][cage-1]) + 0.082*(log(wt)-0.55)
+Einf = (exp(eta_aldrin)/(1+exp(eta_aldrin)))*tau*sum(cur_cage['stage']==2)
 
+#Mating events---------------------------------------------------------------------------------
