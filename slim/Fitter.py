c7e6c9a84519719437b502a735fbc86206adcdc5
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2022-05-12 23:44:56 +0100

    Made fitter params cli-based

--- slim/Fitter.py
+++ slim/Fitter.py
@@ -3,7 +3,7 @@
 """
 Fit to real data
 """
-import functools
+import argparse
 import pickle
 from datetime import timedelta
 import os
@@ -41,72 +41,77 @@ def load_report(config):
     return load_counts(config)
 
 
-def trainable(config: dict):
-    os.chdir(
-        os.environ["TUNE_ORIG_WORKING_DIR"]
-    )  # See: https://github.com/ray-project/ray/pull/22803
-    slim_config = Config(
-        "config_data/config.json",
-        "config_data/Fyne",
-        save_rate=0,
-        farms_per_process=1,
-    )
-    print(config["fish_mortality_k"])
-    print(config["fish_mortality_center"])
-    # TODO: support for nested property updates would be nice...
-    treatment = slim_config.get_treatment(Treatment.EMB)
-    treatment.pheno_resistance = {
-        HeterozygousResistance.DOMINANT: config[
-            "EMB_pheno_resistance_homozygous_dominant"
-        ],
-        HeterozygousResistance.INCOMPLETELY_DOMINANT: config[
-            "EMB_pheno_resistance_heterozygous_dominant"
-        ],
-        HeterozygousResistance.RECESSIVE: config[
-            "EMB_pheno_resistance_homozygous_recessive"
-        ],
-    }
+def trainable(environment):
+    def _trainable(config: dict):
+        os.chdir(
+            os.environ["TUNE_ORIG_WORKING_DIR"]
+        )  # See: https://github.com/ray-project/ray/pull/22803
+        slim_config = Config(
+            "config_data/config.json",
+            environment,
+            save_rate=0,
+            farms_per_process=1,
+        )
+        # TODO: support for nested property updates would be nice...
+        treatment = slim_config.get_treatment(Treatment.EMB)
+        treatment.pheno_resistance = {
+            HeterozygousResistance.DOMINANT: config[
+                "EMB_pheno_resistance_homozygous_dominant"
+            ],
+            HeterozygousResistance.INCOMPLETELY_DOMINANT: config[
+                "EMB_pheno_resistance_heterozygous_dominant"
+            ],
+            HeterozygousResistance.RECESSIVE: config[
+                "EMB_pheno_resistance_homozygous_recessive"
+            ],
+        }
 
-    days = 450
+        days = 450
 
-    slim_config.end_date = slim_config.start_date + timedelta(days=days)
-    simulator = get_env(slim_config)
-    strategy = load_strategy()
-    simulator.reset()
+        slim_config.end_date = slim_config.start_date + timedelta(days=days)
+        simulator = get_env(slim_config)
+        strategy = load_strategy()
+        simulator.reset()
 
-    report_df = load_report(slim_config)
-    counts_per_farm = (
-        report_df.groupby("site_name")[["survived_fish", "lice_count", "date"]]
-        .apply(lambda x: x.reset_index().to_dict())
-        .to_dict()
-    )
+        report_df = load_report(slim_config)
+        counts_per_farm = (
+            report_df.groupby("site_name")[["survived_fish", "lice_count", "date"]]
+            .apply(lambda x: x.reset_index().to_dict())
+            .to_dict()
+        )
 
-    loss = 0.0
-    for _ in tqdm.trange(days):
-        for agent in simulator.agents:
-            action = strategy.predict(None, agent)
-            simulator.step(action)  # note: this assumes these two agent lists match...
+        loss = 0.0
+        for _ in tqdm.trange(days):
+            for agent in simulator.agents:
+                action = strategy.predict(None, agent)
+                simulator.step(
+                    action
+                )  # note: this assumes these two agent lists match...
 
-        simulator_pz: SimulatorPZEnv = simulator.unwrapped
+            simulator_pz: SimulatorPZEnv = simulator.unwrapped
 
-        last_fish_pop = simulator_pz._fish_pop[-1]
-        last_lice_pop = simulator_pz._adult_females_agg[-1]
+            last_fish_pop = simulator_pz._fish_pop[-1]
+            last_lice_pop = simulator_pz._adult_females_agg[-1]
 
-        for agent in range(simulator.num_agents):
-            agent_name = slim_config.farms[agent].name
-            farm_df = counts_per_farm.get(agent_name)
+            for agent in range(simulator.num_agents):
+                agent_name = slim_config.farms[agent].name
+                farm_df = counts_per_farm.get(agent_name)
 
-            day = simulator_pz.cur_day
-            idx = pd.Series(farm_df["date"]).searchsorted(day)
-            delta_fish = farm_df["survived_fish"][idx] - last_fish_pop[f"farm_{agent}"]
-            delta_lice = farm_df["lice_count"][idx] - last_lice_pop[f"farm_{agent}"]
-            if np.isnan(delta_fish):
-                delta_fish = 0.0
-            if np.isnan(delta_lice):
-                delta_lice = 0.0
-            loss += delta_fish**2 + delta_lice**2
+                day = simulator_pz.cur_day
+                idx = pd.Series(farm_df["date"]).searchsorted(day)
+                delta_fish = (
+                    farm_df["survived_fish"][idx] - last_fish_pop[f"farm_{agent}"]
+                )
+                delta_lice = farm_df["lice_count"][idx] - last_lice_pop[f"farm_{agent}"]
+                if np.isnan(delta_fish):
+                    delta_fish = 0.0
+                if np.isnan(delta_lice):
+                    delta_lice = 0.0
+                loss += delta_fish**2 + delta_lice**2
 
-    tune.report(mean_loss=loss)
+        tune.report(mean_loss=loss)
+
+    return _trainable
 
 
 search_space = {
@@ -123,7 +128,7 @@ search_space = {
 }
 
 
-def main():
+def main(environment, logdir=None, num_samples=-1):
     """
     algo = TuneBOHB(metric="mean_loss", mode="min")
     scheduler = HyperBandForBOHB(
@@ -136,12 +141,13 @@ def main():
     scheduler = None
 
     analysis = tune.run(
-        trainable,
+        trainable(environment),
+        local_dir=logdir,
         name="SLIM Fitter",
         # config=search_space,
         scheduler=scheduler,
         search_alg=algo,
-        num_samples=1000,
+        num_samples=num_samples,
     )
     best_result = analysis.get_best_logdir("mean_loss", mode="min")
     print(best_result)
@@ -150,4 +156,16 @@ def main():
 if __name__ == "__main__":
     # print(search_space)
     # trainable(search_space)
-    main()
+
+    parser = argparse.ArgumentParser("SLIM Fitter")
+    parser.add_argument("environment", help="The environment to use")
+    parser.add_argument(
+        "--num_samples",
+        help="The number of iterations. -1 = infinite",
+        type=int,
+        default=-1,
+    )
+    parser.add_argument("--logdir", help="The output logging directory.")
+
+    args = parser.parse_args()
+    main(args.environment, logdir=args.logdir, num_samples=args.num_samples)
